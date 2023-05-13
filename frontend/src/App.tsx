2a2179f40f82e00fd261949ddcd8f9ad6d9ba583
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-05-13 11:05:09 +0530

    mental health page fully functional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Home from "./Pages/Home/Home";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Mentalhelth from './Pages/Mentalhelth';
+
 function App() {
   return (
     <div className="App">
-     {/* <Navbar />
+     <Navbar />
       <Home />
-      <Footer /> */}
-      <Mentalhelth/>
+      <Footer />
+    
     </div>
   );
 }
