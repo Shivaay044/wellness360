f1d0dd0a74ef74f52f36b3a9637c1cfb877b1bba
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-05-13 12:10:34 +0530

    mental health route added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,14 @@ import './App.css';
 import Physical from './Pages/PhysicalFitness/Physical';
 import Exercise from './Pages/PhysicalFitness/Exercise';
 import AllRoute from './components/AllRoute';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-    <AllRoute/>
+      <AllRoute/>
   </div> )
 }
 
--- frontend/src/components/AllRoute.tsx
+++ frontend/src/components/AllRoute.tsx
@@ -5,11 +5,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Physical from "../Pages/PhysicalFitness/Physical";
 import Exercise from "../Pages/PhysicalFitness/Exercise";
-
+import Mentalhelth from "../Pages/Mentalhelth";
+import Home from "../Pages/Home/Home";
 function AllRoute() {
   return <Routes>
     <Route path="/fit" element={<Physical/>}/>
     <Route path="/exe" element={<Exercise/>}/>
+    <Route path="/mentalhealth" element={<Mentalhelth/>}/>
   </Routes>
   
   ;
